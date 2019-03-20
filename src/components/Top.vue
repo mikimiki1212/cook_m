134a19ee612f7c5e8ce7c3f78c6af728f6e5e032
yuma300 <you@example.com>
2019-03-21 02:39:46 +0900

    TOPページ サービス紹介ページ 事業内容ページ 会社情報ページ 上記ページの用意

---
+++ src/components/Top.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li><router-link to="/company">Go To Company Page</router-link></li>
+      <li><router-link to="/business">Go To Business Page</router-link></li>
+      <li><router-link to="/service">Go To Service Page</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top',
+  data () {
+    return {
+      msg: 'Welcome to cook_m'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
+import Business from '@/components/Business'
+import Company from '@/components/Company'
+import Service from '@/components/Service'
 
 Vue.use(Router)
 
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Top',
+      component: Top
+    },
+    {
+      path: '/company',
+      name: 'Company',
+      component: Company
+    },
+    {
+      path: '/business',
+      name: 'Business',
+      component: Business
+    },
+    {
+      path: '/service',
+      name: 'Service',
+      component: Service
     }
   ]
 })
